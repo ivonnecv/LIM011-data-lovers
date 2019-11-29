ade9a207cdb5ec99cba028e889650e4aae548b4a
Ivonne Cervantes <madameiv.264@gmail.com>
2019-11-29 07:25:07 -0500

    readme

--- src/main.js
+++ src/main.js
@@ -4,16 +4,14 @@ import {
   traerDataPokemon, orderAz, orderZa, filterType, orderTopTenAvg, filterWeaknesses,
 } from './data.js';
 
-const dataModificada = traerDataPokemon(POKEMON); // ARRAY DE objetos con 3 caracteristicas;
+const dataModificada = traerDataPokemon(POKEMON); // ARRAY DE objetos con 6 caracteristicas;
 const seccionCard = document.querySelector('#seccion-card');
 const pokeaz = orderAz(dataModificada);
 const pokeza = orderZa(dataModificada);
-// const poketipo = filterType(POKEMON);
 const poketop10 = orderTopTenAvg(dataModificada);
 
 const az = document.querySelector('#az');
 const za = document.querySelector('#za');
-// const tipo = document.querySelector('#tipo');
 const top10 = document.querySelector('#top10');
 
 // clear card
@@ -28,7 +26,7 @@ function pokeLayout(data) {
   let stringTemplate = '';
   for (let i = 0; i < data.length; i += 1) {
     stringTemplate += `
-                          <div id='poke${data[i].id}' class='card'>
+                          <div id='poke${data[i].id}' class="card ${data[i].type[0]}">
                               <div class='card-items'> 
                                   <h2>${data[i].name}</h2>
                                   <h3>${data[i].num}</h3>
@@ -63,14 +61,14 @@ top10.addEventListener('click', () => {
 // traer elemento actual donde ocurre el evento
 const submenuType = document.querySelector('#submenuType');
 submenuType.addEventListener('click', (event) => {
-  clearCard();
+  clearCard(); // función limpiar arriba
   const getAttr = event.target.getAttribute('data-type');
   pokeLayout(filterType(getAttr, dataModificada));
 });
 
 const submenuWeaknesses = document.querySelector('#submenuWeaknesses');
 submenuWeaknesses.addEventListener('click', (event) => {
-  clearCard();
+  clearCard(); // funcion limpiar arriba
   const getAttr = event.target.getAttribute('data-debilidad');
   pokeLayout(filterWeaknesses(getAttr, dataModificada));
 });
@@ -99,63 +97,69 @@ navbarLinks.forEach((elem) => elem.addEventListener('click', () => {
   }
 }));
 
-
-
 /* MODAL  */
 
-let button_modal = document.querySelector('#verPokemon_1');
-let modal_container = document.querySelector('.modal-container');
-let modal_close = document.querySelector('.modal-close');
+// const button_modal = document.querySelector('#verPokemon');
+const modalContainer = document.querySelector('.modal-container');
+const modalClose = document.querySelector('.modal-close');
 
 
-let pokemodal_name = document.querySelector('#pokemodal-name');
-let pokemodal_num = document.querySelector('#pokemodal-num');
-let pokemodal_candys = document.querySelector('#pokemodal-candys');
-let pokemodal_img = document.querySelector('#pokemodal-img');
+const pokemodalName = document.querySelector('#pokemodal-name');
+const pokemodalNum = document.querySelector('#pokemodal-num');
+const pokemodalCandys = document.querySelector('#pokemodal-candys');
+const pokemodalImg = document.querySelector('#pokemodal-img');
+const pokemodalAvg = document.querySelector('#pokemodal-spawns');
+const pokemodalType = document.querySelector('#pokemodal-type');
+const pokemodalWeaknesses = document.querySelector('#pokemodal-weaknesses');
+// const pokemodalPrevEvolution = document.querySelector('#pokemodal-pre-evolution');
+// const pokemodalNextEvolution = document.querySelector('#pokemodal-next-evolution');
 
+const pokeContainer = document.querySelector('#seccion-card');
 
-button_modal.addEventListener('click', (event) => {
+pokeContainer.addEventListener('click', (event) => {
+  const getDataId = event.target.getAttribute('data-id');
+  // button_modal.addEventListener('click', (event) => {
+  // let getDataId = event.target.getAttribute('data-id');
 
-//  alert('Modal 1');
+  modalContainer.classList.remove('modal-hide');
+  modalContainer.classList.add('modal-show');
 
-  modal_container.classList.remove('modal-hide');
-  modal_container.classList.add('modal-show');
+  /* Insert data */
 
-  /*Insert data*/
+  // console.log(dataModificada);
 
-   let get_data_id = event.target.getAttribute('data-id');
-    
-   //console.log(dataModificada);
+  for (let i = 0; i < dataModificada.length; i += 1) {
+    if (dataModificada[i].id === parseInt(getDataId, 10)) {
+      const dataName = dataModificada[i].name;
+      const dataNum = dataModificada[i].num;
+      const dataCandyCount = dataModificada[i].candy_count;
+      const dataAvgSpawns = dataModificada[i].avg_spawns;
+      const dataType = dataModificada[i].type;
+      const dataWeaknesses = dataModificada[i].weaknesses;
+      const dataImg = dataModificada[i].img;
+      /*
+      let dataPrevEvolution = dataModificada[i].id - 1;
+      let dataNextEvolution = dataModificada[i].id + 1;
+      */
 
-   for (let i = 0; i < dataModificada.length; i += 1 ) {
-      if (dataModificada[i].id == parseInt(get_data_id)) {
-        
-        let data_name = dataModificada[i].name;
-        let data_num = dataModificada[i].num;
-        let data_candy_count = dataModificada[i].candy_count;
-        
-        let data_img = dataModificada[i].img;
+      pokemodalName.innerHTML = dataName;
+      pokemodalNum.innerHTML = dataNum;
+      pokemodalCandys.innerHTML = dataCandyCount;
+      pokemodalAvg.innerHTML = dataAvgSpawns;
+      pokemodalType.innerHTML = dataType;
+      pokemodalWeaknesses.innerHTML = dataWeaknesses;
+      pokemodalImg.setAttribute('src', dataImg);
+      // pokemodalPrevEvolution.setAttribute('src', urlPrev);
+      // pokemodalNextEvolution.setAttribute('src', urlNext);
 
-        pokemodal_name.innerHTML = data_name;
-        pokemodal_num.innerHTML = data_num;
-        pokemodal_candys.innerHTML = data_candy_count;
-
-        pokemodal_img.setAttribute('src', data_img);
-        
-        
-        //if (dataModificada[i].prev_evolution == NULL) {
-          
-        //}
-
-      }
-   }
-
-  
-
+      /* if (dataModificada[i].prev_evolution === UNDEFINED) {
+      // pre_evolution = 0
+      // } */
+    }
+  }
 });
 
-modal_close.addEventListener('click', () => {
-  modal_container.classList.remove('modal-show');
-  modal_container.classList.add('modal-hide');
+modalClose.addEventListener('click', () => {
+  modalContainer.classList.remove('modal-show');
+  modalContainer.classList.add('modal-hide');
 });
-
